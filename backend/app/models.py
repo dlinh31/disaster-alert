7beb3c759e279923b487800a473f72156f07f674
Dung Pham <phamanhdung250204@gmail.com>
2024-09-28 03:59:46 -0400

    Geo Data

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,3 +9,21 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.name}>'
+
+
+class Flood_Alert(db.Model):
+    _tablename_ = "flood_alert"
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.String(100), nullable=False)
+    area_desc = db.Column(db.String(255), nullable=False)
+    severity = db.Column(db.String(50), nullable=False)
+    certainty = db.Column(db.String(50), nullable=False)
+    urgency = db.Column(db.String(50), nullable=False)
+    headline = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    coordinates = db.Column(db.Text, nullable=False)
+    effective = db.Column(db.DateTime, nullable=False)
+    expires = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"<Alert {self.event}>"
