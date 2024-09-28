602d2ce10422ccefca506ee2710cb8db46f55167
Hieu Tran <tranthanhhieu982@gmail.com>
2024-09-28 07:16:02 -0400

    up to date

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -126,57 +126,6 @@ function Map() {
           )}
         </GoogleMap>
       </Box>
-
-      <Box
-        p={4}
-        borderRadius="lg"
-        m={4}
-        bgColor="white"
-        shadow="base"
-        minW="container.md"
-        zIndex="1"
-      >
-        <HStack spacing={2} justifyContent="space-between">
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input type="text" placeholder="Origin" ref={originRef} />
-            </Autocomplete>
-          </Box>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                type="text"
-                placeholder="Destination"
-                ref={destinationRef}
-              />
-            </Autocomplete>
-          </Box>
-
-          <ButtonGroup>
-            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
-              Calculate Route
-            </Button>
-            <IconButton
-              aria-label="center back"
-              icon={<FaTimes />}
-              onClick={clearRoute}
-            />
-          </ButtonGroup>
-        </HStack>
-        <HStack spacing={4} mt={4} justifyContent="space-between">
-          <Text>Distance: {distance} </Text>
-          <Text>Duration: {duration} </Text>
-          <IconButton
-            aria-label="center back"
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => {
-              map.panTo(center);
-              map.setZoom(12);
-            }}
-          />
-        </HStack>
-      </Box>
     </Flex>
   );
 }
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Map from '../components/Map';
+import DisasterCard from '../components/DisasterCard';
+import Navbar from '../components/Navbar'; // Ensure Navbar is imported
+import { Grid, GridItem } from "@chakra-ui/react";
+
 const Home = () => {
   return (
-    <div className="flex flex-col px-3 items-center w-full">
-      <div className="w-full h-20vh">Navbar</div>
-      <div className='p-5 h-80vh'>
+    <Grid
+      templateAreas={`"header header"
+                      "nav main"`}
+      gridTemplateRows={'auto 1fr'} // 'auto' for header and '1fr' for main
+      gridTemplateColumns={'25vw 1fr'} // Updated nav width (25%)
+      height='100vh' // Occupies the full height of the viewport
+      width='100vw' // Occupies the full width of the viewport
+      gap='0' // No gaps between items
+      overflow='hidden' // Prevent overflow causing scroll bars
+    >
+      <GridItem area={'header'}>
+        <Navbar />
+      </GridItem>
+      <GridItem area={'nav'}>
+        <DisasterCard />
+      </GridItem>
+      <GridItem area={'main'}>
         <Map />
-      </div>
-    </div>
+      </GridItem>
+    </Grid>
   );
 };
 
